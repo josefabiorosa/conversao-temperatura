928a873ee10e57d837787f061e5e1e2270fd150a
Jose Fabio Rosa <josefabiorosa@gmail.com>
2025-04-30 16:47:52 -0300

    atualizando

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ app.use('/', config.routers);
 app.use(bodyParser.urlencoded({ extended: false }))
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+//teste
 app.get('/fahrenheit/:valor/celsius', (req, res) => {
 
     let valor = req.params.valor;
